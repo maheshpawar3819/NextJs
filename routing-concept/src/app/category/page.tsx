62fca298ebfa9fd8345226080367d7dd248152db
maheshpawar3819 <pawarmahesh3819@gmail.com>
2025-02-17 21:53:46 +0530

    add practice code of routing concept

---
+++ routing-concept/src/app/category/page.tsx
@@ -0,0 +1,3 @@
+export default function Categories() {
+  return <h1>Welcome to Categories page</h1>;
+}
---
+++ routing-concept/src/app/contact-us/page.tsx
@@ -0,0 +1,3 @@
+export default function ContactUs(){
+    return <h1>Welcome to contact us page</h1>
+}
