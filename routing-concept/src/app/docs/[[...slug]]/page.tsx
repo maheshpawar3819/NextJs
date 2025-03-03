b5fae3e2ec3889bfd3569397ae2d1e871b41abee
maheshpawar3819 <pawarmahesh3819@gmail.com>
2025-03-03 23:57:53 +0530

    Catch all

---
+++ routing-concept/src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,21 @@
+export default async function Docs({
+  params,
+}: {
+  params: Promise<{ slug: string[] }>;
+}) {
+  const { slug } = await params;
+  if (slug?.length === 2) {
+    return (
+      <h1>
+        Viwing docs for feature {slug[0]} and Concept {slug[1]}
+      </h1>
+    );
+  } else if (slug?.length === 1) {
+    return <h1>Viwing docs for feature {slug[0]} </h1>;
+  }
+  return (
+    <>
+      <h1>Docs Home Page</h1>
+    </>
+  );
+}
---
+++ routing-concept/src/app/items/[itemsId]/reviews/page.tsx
@@ -0,0 +1,9 @@
+export default function Reviews() {
+    return <>
+        <h1>List Of Reviews</h1>
+        <p>Good</p>
+        <p>Very Good</p>
+        <p>Nice</p>
+        <p>Beautiful-</p>
+    </>
+}
